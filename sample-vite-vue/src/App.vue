f7feb39c60f0dc51d145fe0cba6964e985da4b57
zigoro <zigoro@chopchopmaster.com>
2023-05-04 17:39:26 +0900

    1st commit

---
+++ sample-vite-vue/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import MainContent from './components/MainContent.vue';
+</script>
+
+<template>
+  <MainContent></MainContent>
+
+  <footer class="bg-sky-100 text-center dark:bg-sky-700 lg:text-left">
+    <div class="p-4 text-center text-neutral-700 dark:text-neutral-200">
+      © 2023 Copyright:
+      <a class="text-neutral-800 dark:text-neutral-400" href="https://tailwind-elements.com/">Tailwind Elements</a>
+    </div>
+  </footer>
+</template>
+
+<style scoped></style>
---
+++ sample-vite-vue/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,vue}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class"
+}
+
